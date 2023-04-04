6e7c95b23dffa14fce9beeb64447315233960670
matteogiud <98876848+matteogiud@users.noreply.github.com>
2023-04-04 09:54:15 +0200

    null

--- pigriPy/testPyCharm/app.py
+++ pigriPy/testPyCharm/app.py
@@ -1,6 +1,6 @@
-from machine import Pin
+from machine import Pin, PWM
 from motorsDriver import MotorsDriver
 
 
-MD = MotorsDriver(Pin(27, Pin.OUT), Pin(14, Pin.OUT), Pin(33, Pin.OUT), Pin(32, Pin.OUT), Pin(26, Pin.OUT), Pin(25, Pin.OUT))
+MD = MotorsDriver(Pin(27, Pin.OUT), Pin(14, Pin.OUT), Pin(33, Pin.OUT), Pin(32, Pin.OUT), PWM(Pin(26, Pin.OUT)), PWM(Pin(25, Pin.OUT)))
 MD.forward()
--- pigriPy/testPyCharm/motorsDriver.py
+++ pigriPy/testPyCharm/motorsDriver.py
@@ -1,12 +1,15 @@
 class MotorsDriver:
 
-    def __init__(self, inPin1, inPin2, inPin3, inPin4, enPinA, enPinB) -> None:
+    def __init__(self, inPin1, inPin2, inPin3, inPin4, enPinA, enPinB, diameterWheelA = 67, diameterWheelB = 67) -> None:
         self.inPin1 = inPin1
         self.inPin2 = inPin2
         self.enPinA = enPinA
         self.inPin3 = inPin3
         self.inPin4 = inPin4
         self.enPinB = enPinB
+        self.diameterWheelA = diameterWheelA
+        self.diameterWheelB = diameterWheelB
+        self.pwmVelocity = 255
 
 
     def forward(self) -> None:
@@ -14,16 +17,16 @@ class MotorsDriver:
         self.inPin2.value(0)
         self.inPin3.value(1)
         self.inPin4.value(0)
-        self.enPinA.value(1)
-        self.enPinB.value(1)
+        self.enPinA.value(self.pwmVelocity)
+        self.enPinB.value(self.pwmVelocity)
 
     def backward(self) -> None:
         self.inPin1.value(0)
         self.inPin2.value(1)
-        self.enPinA.value(1)
+        self.enPinA.value(self.pwmVelocity)
         self.inPin3.value(0)
         self.inPin4.value(1)
-        self.enPinB.value(1)
+        self.enPinB.value(self.pwmVelocity)
 
     def stop(self) -> None:
         self.inPin1.value(0)
