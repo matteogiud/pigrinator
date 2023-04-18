ab004a69ea8144a094abaa1ed21c3bc9bf279a7f
matteogiud <matteogiud@icloud.com>
2023-04-18 11:42:31 +0200

    distanza in base alle ruote

--- TEST/ESP32/stepper_motor/lib/Stepper.py
+++ TEST/ESP32/stepper_motor/lib/Stepper.py
@@ -1,6 +1,7 @@
 import machine
 import utime
 
+
 class StepperMotor:
     def __init__(self, pin1, pin2, pin3, pin4):
         # Definiamo i pin per controllare il motore stepper
@@ -10,50 +11,53 @@ class StepperMotor:
         self.coil_B_2_pin = machine.Pin(pin4, machine.Pin.OUT)
 
         # Definiamo la sequenza dei pin di controllo del motore stepper
-        self.seq = [[1,0,0,1],
-                    [1,0,0,0],
-                    [1,1,0,0],
-                    [0,1,0,0],
-                    [0,1,1,0],
-                    [0,0,1,0],
-                    [0,0,1,1],
-                    [0,0,0,1]]
+        self.seq = [[1, 0, 0, 1],
+                    [1, 0, 0, 0],
+                    [1, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 1, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 1, 1],
+                    [0, 0, 0, 1]]
 
     # Definiamo la funzione per far girare il motore
     def step(self, delay, steps):
         for i in range(steps):
             for halfstep in range(8):
                 for pin in range(4):
-                    if self.seq[halfstep][pin]!=0:
-                        if pin==0:
+                    if self.seq[halfstep][pin] != 0:
+                        if pin == 0:
                             self.coil_A_1_pin.on()
-                        elif pin==1:
+                        elif pin == 1:
                             self.coil_A_2_pin.on()
-                        elif pin==2:
+                        elif pin == 2:
                             self.coil_B_1_pin.on()
-                        elif pin==3:
+                        elif pin == 3:
                             self.coil_B_2_pin.on()
                     else:
-                        if pin==0:
+                        if pin == 0:
                             self.coil_A_1_pin.off()
-                        elif pin==1:
+                        elif pin == 1:
                             self.coil_A_2_pin.off()
-                        elif pin==2:
+                        elif pin == 2:
                             self.coil_B_1_pin.off()
-                        elif pin==3:
+                        elif pin == 3:
                             self.coil_B_2_pin.off()
                 utime.sleep_us(delay)
 
     # Definiamo la funzione per far girare il motore in una direzione specifica
     def rotate(self, direction, speed, degrees):
-        steps = int(degrees/0.0879) # calcoliamo il numero di steps necessari per ruotare di n gradi
-        delay = int(1000000/speed) # calcoliamo il ritardo necessario per la velocità desiderata
+        # calcoliamo il numero di steps necessari per ruotare di n gradi
+        steps = int(degrees/0.0879)
+        # calcoliamo il ritardo necessario per la velocità desiderata
+        delay = int(1000000/speed)
         if direction == "CW":
             self.step(delay, steps)
         elif direction == "CCW":
-            self.seq = list(reversed(self.seq)) # invertiamo la sequenza per far girare il motore in senso antiorario
+            # invertiamo la sequenza per far girare il motore in senso antiorario
+            self.seq = list(reversed(self.seq))
             self.step(delay, steps)
-            self.seq = list(reversed(self.seq)) # ripristiniamo la sequenza
+            self.seq = list(reversed(self.seq))  # ripristiniamo la sequenza
 
     # Definiamo la funzione per far girare il motore in entrambe le direzioni
     def oscillate(self, speed, degrees):
@@ -66,37 +70,38 @@ class StepperMotor:
         self.coil_A_2_pin.off()
         self.coil_B_1_pin.off()
         self.coil_B_2_pin.off()
-        
-        
 
 
 class CarMotors:
-    def __init__(self, motor_A, motor_B):
+    def __init__(self, motor_A: StepperMotor, motor_B: StepperMotor, wheels_diameter=0.067, steps_per_rotation=4096):
         # Inizializziamo i due motori stepper
-        print(motor_A, motor_B)
         self.motor_A = motor_A
         self.motor_B = motor_B
-    
+        self.steps_per_rotation = steps_per_rotation
+        self.wheels_diameter = wheels_diameter
+
     # Definiamo la funzione per far avanzare la macchinina
-    def forward(self, speed, distance):
-        print(self.motor_A)
-        degrees = distance / 0.13 # calcoliamo il numero di gradi necessari per percorrere una certa distanza
+    def forward(self, distance, speed=1024):
+        """Ruota le ruote in senso orario di una distanza in centimetri"""
+        import math
+        degrees = distance * 360 / (self.wheel_diameter * math.pi)
         self.motor_A.rotate("CW", speed, degrees)
         self.motor_B.rotate("CCW", speed, degrees)
 
     # Definiamo la funzione per far retrocedere la macchinina
-    def backward(self, speed, distance):
-        degrees = distance / 0.13 # calcoliamo il numero di gradi necessari per percorrere una certa distanza
+    def backward(self, distance, speed=1024):
+        import math
+        degrees = distance * 360 / (self.wheel_diameter * math.pi)
         self.motor_A.rotate("CCW", speed, degrees)
         self.motor_B.rotate("CW", speed, degrees)
 
     # Definiamo la funzione per girare a destra
-    def turn_right(self, speed, degrees):
+    def turn_right(self, degrees, speed=1024):
         self.motor_A.rotate("CW", speed, degrees)
         self.motor_B.rotate("CW", speed, degrees)
 
     # Definiamo la funzione per girare a sinistra
-    def turn_left(self, speed, degrees):
+    def turn_left(self, degrees, speed=1024):
         self.motor_A.rotate("CCW", speed, degrees)
         self.motor_B.rotate("CCW", speed, degrees)
 
@@ -104,4 +109,3 @@ class CarMotors:
     def stop(self):
         self.motor_A.stop()
         self.motor_B.stop()
-
--- TEST/ESP32/stepper_motor/main.py
+++ TEST/ESP32/stepper_motor/main.py
@@ -2,9 +2,9 @@ from lib.Stepper import StepperMotor, CarMotors
 from machine import Pin
 
 step_motor_A = StepperMotor(26, 25, 33, 32)
-step_motor_B = StepperMotor(16, 17, 05, 18)
+step_motor_B = StepperMotor(16, 17, 5, 18)
 
 car_motors = CarMotors(step_motor_A, step_motor_B)
 
 
-# car_motors.forward(1024,1000)
+car_motors.forward(10)
