8587ed58238f1956f4e23fce215345de23577e72
gio04R <118894256+gio04R@users.noreply.github.com>
2023-04-18 08:08:56 +0200

    update
    update

--- pigra/app/muoviti.java
+++ pigra/app/muoviti.java
@@ -9,45 +9,7 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class muoviti {
 
-                public static void main(String[] args) {
-                    String topic        = "esp32/sensors";
-                    String broker       = "tcp://esp32.local:1883"; // sostituisci con l'indirizzo IP o hostname del tuo ESP32
-                    String clientId     = "JavaClient";
-                    MemoryPersistence persistence = new MemoryPersistence();
+    public static void main(String[] args) {
 
-                    try {
-                        // creiamo un'istanza del client MQTT e lo connettiamo al broker
-                        MqttClient client = new MqttClient(broker, clientId, persistence);
-                        MqttConnectOptions connOpts = new MqttConnectOptions();
-                        connOpts.setCleanSession(true);
-                        System.out.println("Connettendo al broker: "+broker);
-                        client.connect(connOpts);
-                        System.out.println("Connesso");
-
-                        // iscriviamo il client al topic dei sensori dell'ESP32
-                        System.out.println("Iscritto al topic "+topic);
-                        client.subscribe(topic);
-
-                        // definiamo un listener per ricevere i messaggi dal broker
-                        client.setCallback(new MqttCallback() {
-                            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                                // gestiamo il messaggio ricevuto dal broker
-                                System.out.println("Messaggio ricevuto:");
-                                System.out.println("\tTopic: "+topic);
-                                System.out.println("\tMessage: "+new String(message.getPayload()));
-                            }
-
-                            public void deliveryComplete(IMqttDeliveryToken token) {
-                            }
-
-                            public void connectionLost(Throwable cause) {
-                                System.out.println("Connessione persa: "+cause.getMessage());
-                            }
-                        });
-
-                    } catch(MqttException me) {
-                        System.out.println("Eccezione: "+me.getMessage());
-                        me.printStackTrace();
-                    }
-                }
-            }
+    }
+}
--- pigra/app/src/main/java/com/example/pigra/MainActivity.java
+++ pigra/app/src/main/java/com/example/pigra/MainActivity.java
@@ -11,6 +11,16 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +37,34 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+            }
+        });
+        Button evento = (Button) findViewById(R.id.button);
+        evento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String espIp = "http://192.168.1.100/forward";
+
+                URL obj = null;
+
+                try {
+                    obj = new URL(espIp);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                    con.setRequestMethod("GET");
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuilder response=new StrignBuilder();
+                    while((inputLine=in.readLine())!= null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    System.out.println(response.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
             }
         });
     }
