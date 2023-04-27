bdcc012b537ff962570055ce4bec85620c1fd526
matteogiud <matteogiud@icloud.com>
2023-04-27 15:05:50 +0200

    new path endpoint

--- TEST/ACTUAL FLASH (WIFI CONNECTION HANDLING ALSO WITH ROBOT)/lib/paths_handler.py
+++ TEST/ACTUAL FLASH (WIFI CONNECTION HANDLING ALSO WITH ROBOT)/lib/paths_handler.py
@@ -1,5 +1,6 @@
 import json
 
+
 def readFile():
     try:
         with open('paths.json', 'r') as file:
@@ -7,32 +8,61 @@ def readFile():
         return data
     except Exception as e:
         print(e)
-        
-def loadWifi(paths={}) -> None:
+
+
+def loadPath(path={}) -> bool:
+    if not checkPath(path=path):
+        return False
     try:
         with open('paths.json', 'r') as file:
             data = json.load(file)
-        
+
         new_id = max(map(int, data['paths'].keys())) + 1
-        
-              
-        data[str(new_id)]= paths
-        
+
+        data[str(new_id)] = path
+
         with open('paths.json', 'w') as file:
             json.dump(data, file)
         print(data)
 
     except Exception as e:
         print(e)
+        return False
     
+    return True
+
+def checkPath(path={}):
+    if not path:
+        return False
+    valid_keys = ("forward", "backward", "left", "right")
+    try:           
+        # x:dict = {"forward": 100}
+        for step in path:
+            if not len(step) == 2:
+                return False
+            
+            if not all(key in step for key in ["direction", "value"]):
+                return False
+            
+            direction = step["direction"]
+            if direction not in ["forward", "backward", "left", "right"]:
+                return False
+            
+            value = step["value"]
+            if not isinstance(value, int) or value <= 0:
+                return False            
+            
+    except:
+        return False
+    
+    return True
 
 def search(path_id) -> str:
     try:
         with open('paths.json', 'r') as file:
             data = json.load(file)
-            
+
         return data["paths"][str(path_id)]
     except Exception as e:
         print("[paths_handler]: ", e)
         return None
-
--- TEST/ACTUAL FLASH (WIFI CONNECTION HANDLING ALSO WITH ROBOT)/main.py
+++ TEST/ACTUAL FLASH (WIFI CONNECTION HANDLING ALSO WITH ROBOT)/main.py
@@ -6,6 +6,8 @@ from lib.microdot_utemplate import render_template
 import urequests
 from lib.wifi_manager import WIFIManager
 import global_vars
+import json
+import lib.paths_handler as path
 
 
 
@@ -13,55 +15,71 @@ import global_vars
 # wifiManager.connect()
 
 
-#setup webserver
+# setup webserver
 print(f"[main] car ip: [{global_vars.esp_car_ip_address}]")
 
 app = Microdot()
 
+
+@app.put('/newPath')
+def newPath(req):
+    json_body = json.loads(req.body.decode('utf-8'))
+    print(f"[/newPath] new request: {req.body}")        
+    
+    if not json_body["path"]:        
+        return "bad request, syntax error", 400, {"Content-Type": "text/html"}
+    
+    if not path.loadPath(json_body["path"]):
+        return "bad request, syntax error", 400, {"Content-Type": "text/html"}
+    
+    return "ok", 201, {"Content-Type": "text/html"}
+
+    
+
+
 @app.post('/goTo')
 def goTo(req):
-    print("body: ", req.body)
-    import json; json_body=json.loads(req.body.decode('utf-8'))
+    print("[/goTo]body: ", req.body)
     
-    import lib.paths_handler as paths
+    json_body = json.loads(req.body.decode('utf-8'))
+
     path_id = json_body["path_id"]
-    complete_path = paths.search(path_id) #risolvere: ritorna null
+    complete_path = path.search(path_id)  # risolvere: ritorna null
     if complete_path is None:
         return "bad request", 400, {"Content-Type": "text/html"}
     print(f"[\goTo]: selected path: {complete_path}")
-    car_http_req = f"http://{global_vars.esp_car_ip_address}/followThisPath" #risolvere: prendere ip inviato durante la connessione
+    # risolvere: prendere ip inviato durante la connessione
+    car_http_req = f"http://{global_vars.esp_car_ip_address}/followThisPath"
     # car_http_req = f"http://192.168.1.60/followThisPath"
     # print(car_http_req)
     try:
-        response = urequests.post(car_http_req, headers={"Content-Type": "application/json"}, data=json.dumps(complete_path))
-        if response.status_code == 200: # solleva un'eccezione se la risposta ha uno stato HTTP diverso da 2XX
+        response = urequests.post(car_http_req, headers={
+                                  "Content-Type": "application/json"}, data=json.dumps(complete_path))
+        if response.status_code == 200:  # solleva un'eccezione se la risposta ha uno stato HTTP diverso da 2XX
             print(f"[\goTo]: response status code: {response.status_code}")
             return 'ok', 200, {"Content-Type": "text/html"}
         else:
-            return 'Car is not ready', 503 , {"Content-Type": "text/html"}
+            return 'Car is not ready', 503, {"Content-Type": "text/html"}
 
     except Exception as e:
         print("Errore HTTP:", e)
         # print("Risposta HTTP:", response.text)
-        return 'Car is not ready', 503 , {"Content-Type": "text/html"}
+        return 'Car is not ready', 503, {"Content-Type": "text/html"}
 
-    
-    
-    return 'Car is not ready', 503 , {"Content-Type": "text/html"}
-    
-    
+    return 'Car is not ready', 503, {"Content-Type": "text/html"}
+
+
 @app.get('/')
 def index(req):
     return 'hello'
 
+
 def start_server():
     try:
-        #debug=True, 
+        # debug=True,
         app.run(port=80)
     except:
         app.shutdown()
-        
+
 
 start_server()
-
-
